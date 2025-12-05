d391ac7de46afe96c95a75cc5218285f52f2c68f
Mendbayar <mendbayar45@gmail.com>
2025-12-05 17:54:12 +0800

    update on report

--- report/create_sample.py
+++ report/create_sample.py
@@ -1,7 +1,6 @@
 import pandas as pd
 
-# 1. Load the huge original file (1.6 million rows)
-# Note: encoding='latin-1' is required for Sentiment140
+# Load the huge original file (1.6 million rows)
 print("Loading big dataset...")
 df_full = pd.read_csv(
     "training.1600000.processed.noemoticon.csv", 
@@ -10,11 +9,10 @@ df_full = pd.read_csv(
     names=["target", "id", "date", "flag", "user", "text"]
 )
 
-# 2. THE CRUCIAL STEP: Sampling with a SEED
 # random_state=42 is the "Seed". It ensures we get the exact same rows every time.
 print("Sampling 50,000 rows...")
 df_sample = df_full.sample(n=50000, random_state=42)
 
-# 3. Save the small file to use in your main project
+# Save the small file to use in the main project
 df_sample.to_csv("sample_set.csv", index=False)
 print("Done! Created 'sample_set.csv' with 50,000 rows.")
